c7594854153809b502c88e1a6f37540fe3f40738
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-05-13 18:24:49 -0700

    adds divide and multiply functions externalizes isNumber

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 const greet = require('./lib/greet.js');
 const math = require('./lib/arithmetic.js');
-console.log(greet('JOHN'));
+// console.log(greet('JOHN'));
 console.log(math.add(1,3)); // 4
 console.log(math.subtract(1,3)); // -2
+console.log(math.multiply(2, 3)); // 6
+console.log(math.multiply(1, .5)); // 0.5
+console.log(math.divide(2, 0)); // null
+console.log(math.divide(6, 2)); // 3
